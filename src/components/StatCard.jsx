d65eccc2616781c6d5f1892955277195184d7cd7
MaahinSK <maahin810@gmail.com>
2025-09-28 11:17:08 +0600

    tickt card

---
+++ src/components/StatCard.jsx
@@ -0,0 +1,15 @@
+import "../styles/StatCard.css";
+
+export default function StatCard({ title, count, gradient, img }) {
+  return (
+    <div className={`stat-card ${gradient}`}>
+      <div className="stat-content">
+        <div>
+          <h3>{title}</h3>
+          <p>{count}</p>
+        </div>
+        {img && <img src={img} alt="vector" className="stat-img" />}
+      </div>
+    </div>
+  );
+}
